e6e46134b53072e2f8840a01e3e7618ae01a5afc
Allan Cicceri <allan.cicceri@gmail.com>
2021-10-09 20:02:42 -0300

    Adicionei logo, inputsearch e user avatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,17 @@ import './Header.css'
 
 function Header(){
     return(
-        <header className="headder">
-            <div className="logo">logo</div>
-            <div className="searchContainer">container</div>
-            <div className="user">user</div>
+        <header className="header">
+            <div className="logo">
+                <img src="logob.png" alt="" />
+            </div>
+            <div className="searchContainer">
+                <input type="text" placeholder="search for task..."/>
+            </div>
+            <div className="user">
+                <img src="avatar.png" alt="" />
+                <div className="user-name">User Name</div>
+            </div>
         </header>
     )
 }
