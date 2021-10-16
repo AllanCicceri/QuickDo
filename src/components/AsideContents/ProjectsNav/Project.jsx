7d3c8d6645d0be2ba9b9c377f55e3cac5de18f8a
Allan Cicceri <allan.cicceri@gmail.com>
2021-10-16 17:04:46 -0300

    Criei variaveis para as cores no css

--- src/components/AsideContents/ProjectsNav/Project.jsx
+++ src/components/AsideContents/ProjectsNav/Project.jsx
@@ -5,7 +5,7 @@ import {useDispatch} from 'react-redux'
 
 
 function Project({item}){
-    const style = { color: "#E23C4A", fontSize: "1.5em" }
+    const style = { color: "var(--yellowColor)", fontSize: "1.5em" }
     const dispatch = useDispatch()
 
     const dispatchProject = () => dispatch(ProjectActions.selectProject(item))
